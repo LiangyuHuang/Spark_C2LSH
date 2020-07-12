a24d99d3ad13620bdaec5967d77b72e0b0157c2d
Liangyu <liangyuhuang@outlook.com>
2020-07-12 20:46:54 +1000

    debug for submission.py

--- submission.py
+++ submission.py
@@ -4,15 +4,17 @@
 # do not change the heading of the function
 def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
     offset = -1
-    numCandidate = -1
-    while numCandidate < beta_n:
+    numCandidates = -1
+    while numCandidates < beta_n:
         offset += 1
-        candidatesRDD = data_hashes.filter(lambda e: match(e[2],query_hashes,alpha_m,offset )).map( lambda e: e[0])
+        candidatesRDD = data_hashes.filter(lambda e: match(e[1], query_hashes, alpha_m, offset)).map(lambda e: e[0])
+        numCandidates = candidatesRDD.count()
     return candidatesRDD
+
 
 def match(dataHashCode, queryHashCode, alpha_m, offset):
     count = 0
     for i in range(0, len(dataHashCode)):
-        if abs(dataHashCode - queryHashCode) <= offset:
+        if abs(dataHashCode[i] - queryHashCode[i]) <= offset:
             count += 1
     return count >= alpha_m
