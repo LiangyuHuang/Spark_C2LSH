48066ec7e513a2a4e7497e2566b9a03ad7db1dde
Liangyu <liangyuhuang@outlook.com>
2020-07-13 16:57:41 +1000

    Rewrite code

--- submission.py
+++ submission.py
@@ -2,26 +2,32 @@
 
 ########## Question 1 ##########
 # do not change the heading of the function
+
+
 def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
     offset = -1
     numCandidates = -1
-
-    def differ(data_hash, query_hash):
-        list_different = [abs(data_hash[i] - query_hash[i]) for i in range(0, len(data_hash))]
-        return list_different
-
-    data_hashes = data_hashes.map(lambda e: (e[0], differ(e[1], query_hashes)))
+    data_hashes = data_hashes.map(lambda e: (e[0], differ(e, query_hashes)))
     while numCandidates < beta_n:
         offset += 1
-        candidatesRDD = data_hashes.flatMap(lambda e: [e[0]] if match(e[1], alpha_m, offset) else [])
-        numCandidates = candidatesRDD.count()
-    return candidatesRDD
+        result = data_hashes.flatMap(lambda e: [e[0]] if match(e, alpha_m, offset) else [])
+        numCandidates = result.count()
+    return result
 
 
-def match(list_differ, alpha_m, offset):
+def differ(data_hash, query_hash):
+    list_deal = data_hash[1]
+    list_different = []
+    for i in range(len(list_deal)):
+        list_different.append(abs(list_deal[i] - query_hash[i]))
+    return list_different
+
+
+def match(hash_list, alpha_m, offset):
+    target = hash_list[1]
     count = 0
-    for i in range(0, len(list_differ)):
-        if list_differ[i] <= offset:
+    for i in range(0, len(target)):
+        if target[i] <= offset:
             count += 1
         if count >= alpha_m:
             return True
